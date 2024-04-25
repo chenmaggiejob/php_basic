55537e0c7280c155caee7a4b78f30af36d40ceb3
maggiechen <chenmaggiejob@gmail.com>
2024-04-25 10:27:20 +0800

    子字串取用練習

--- string.php
+++ string.php
@@ -49,6 +49,40 @@
     echo "<br>"
 ?>
 <hr>
-    
+<h2>字串分割</h2>
+<ul>
+    <li>將”this,is,a,book”依”,”切割後成為陣列</li>
+</ul>
+
+<?php
+
+    $s='this,is,a,book';
+    $result=explode(",",$s);
+    echo "<pre>";
+    print_r($result);
+    echo "</pre>";
+
+?>
+<hr>
+<h2>字串組合</h2>
+<ul>
+    <li>將上例陣列重新組合成“this is a book”</li>
+</ul>
+
+<?php
+    $result=join(' ',$result);
+    echo $result;
+?>
+<hr>
+<h2>子字串取用</h2>
+<ul>
+    <li>將” The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…”</li>
+</ul>
+
+<?php
+    $s='The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…';
+    $new=mb_substr($s,0,10);
+    echo $new . '...';
+?>
 </body>
 </html>
